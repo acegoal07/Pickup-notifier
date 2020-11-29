e2761a195ddce096f0133a952b158068b3da51b4
Alexander wood <acegoal07@gmail.com>
2020-11-29 14:57:44 +0000

    update

--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnoti.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnoti.java
@@ -1,11 +1,8 @@
 package com.acegoal07.pickupnoti;
 
-import net.minecraft.init.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = pickupnoti.MODID, name = pickupnoti.NAME, version = pickupnoti.VERSION)
 public class pickupnoti
@@ -13,20 +10,10 @@ public class pickupnoti
     public static final String MODID = "pickupnoti";
     public static final String NAME = "Pickup notifier";
     public static final String VERSION = "1.0.0";
-    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 
-    private static Logger logger;
-
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
-        logger = event.getModLog();
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+        MinecraftForge.EVENT_BUS.register(new pickupnotiEventHandler());
     }
 
-    @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
 }
--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnotiEventHandler.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnotiEventHandler.java
@@ -1,12 +1,11 @@
-package com.acegoal07.pickupnoti.events;
+package com.acegoal07.pickupnoti;
 
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class ClientEvents {
+public class pickupnotiEventHandler {
     @SubscribeEvent
     public void pickupItem(EntityItemPickupEvent event) {
-        System.out.println("Item was successfully picked up");
+        System.out.println(event.getEntityPlayer() + "has picked up" + event.getItem());
     }
 }
