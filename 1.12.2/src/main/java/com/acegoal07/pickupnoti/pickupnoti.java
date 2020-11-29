fdfa56df933dc50db05402447943657bf0eee0b4
Alexander wood <acegoal07@gmail.com>
2020-11-29 16:52:11 +0000

    update

--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnoti.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnoti.java
@@ -9,11 +9,10 @@ public class pickupnoti
 {
     public static final String MODID = "pickupnoti";
     public static final String NAME = "Pickup notifier";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event){
         MinecraftForge.EVENT_BUS.register(new pickupnotiEventHandler());
     }
-
 }
--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnotiEventHandler.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnotiEventHandler.java
@@ -1,11 +1,12 @@
 package com.acegoal07.pickupnoti;
 
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class pickupnotiEventHandler {
     @SubscribeEvent
     public void pickupItem(EntityItemPickupEvent event) {
-        System.out.println(event.getEntityPlayer() + "has picked up" + event.getItem());
+        event.getEntityPlayer().sendMessage(new TextComponentString(event.getEntityPlayer().getName()+" has picked up"+event.getItem().getCustomNameTag()));
     }
 }
