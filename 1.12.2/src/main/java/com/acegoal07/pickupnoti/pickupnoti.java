9fac251d9f85007e5e69232ed573825812bbec14
Alexander wood <acegoal07@gmail.com>
2020-11-30 18:24:29 +0000

    update

--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnoti.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnoti.java
@@ -1,6 +1,5 @@
 package com.acegoal07.pickupnoti;
 
-import com.acegoal07.pickupnoti.events.pickupnotiEventHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -10,7 +9,7 @@ public class pickupnoti
 {
     public static final String MODID = "pickupnoti";
     public static final String NAME = "Pickup Notifier";
-    public static final String VERSION = "1.12.2-1.0.2";
+    public static final String VERSION = "1.12.2-1.0.3";
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event){
--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnotiEventHandler.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/pickupnotiEventHandler.java
@@ -1,4 +1,4 @@
-package com.acegoal07.pickupnoti.events;
+package com.acegoal07.pickupnoti;
 
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
@@ -7,10 +7,11 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class pickupnotiEventHandler {
 
     @SubscribeEvent
-    public void pickupItem(EntityItemPickupEvent event) {
+    public void pickupItem(EntityItemPickupEvent event)
+    {
         String user = event.getEntityPlayer().getName();
         String item = event.getItem().getDisplayName().getFormattedText();
-        event.getEntityPlayer().sendMessage(new TextComponentString(user+" has picked up "+item));
-    }
 
+        event.getEntityPlayer().sendMessage(new TextComponentString(user+" has picked up "+item+" x"));
+    }
 }
