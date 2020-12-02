385df8cf3bae9198e6e842694af36013c4ee0e0d
Alexander wood <acegoal07@gmail.com>
2020-12-02 18:55:39 +0000

    update

--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/events/pickupnotiEventHandler.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/events/pickupnotiEventHandler.java
@@ -1,6 +1,5 @@
 package com.acegoal07.pickupnoti.events;
 
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -10,11 +9,13 @@ public class pickupnotiEventHandler {
     @SubscribeEvent
     public void pickupItem(EntityItemPickupEvent event)
     {
+
         String user = event.getEntityPlayer().getName();
-        String item = event.getItem().getDisplayName().getFormattedText();
 
-        
+        String item = event.getItem().getItem().getDisplayName().toLowerCase();
 
-        event.getEntityPlayer().sendMessage(new TextComponentString(user+" has picked up "+item+" x"));
+        int amount = event.getItem().getItem().getCount();
+
+        event.getEntityPlayer().sendMessage(new TextComponentString(user+" has picked up "+item+" x"+amount));
     }
 }
