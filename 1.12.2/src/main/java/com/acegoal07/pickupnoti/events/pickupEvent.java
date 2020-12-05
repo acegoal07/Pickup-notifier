13460567d9446b5ae49673faa01833dfc773ff4f
Alexander wood <acegoal07@gmail.com>
2020-12-05 11:57:33 +0000

    Update pickupEvent.java

--- 1.12.2/src/main/java/com/acegoal07/pickupnoti/events/pickupEvent.java
+++ 1.12.2/src/main/java/com/acegoal07/pickupnoti/events/pickupEvent.java
@@ -3,6 +3,7 @@ package com.acegoal07.pickupnoti.events;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,8 +16,8 @@ public class pickupEvent {
         String player = event.getEntityPlayer().getName();
         String item = event.getItem().getItem().getDisplayName().toLowerCase();
         int amount = event.getItem().getItem().getCount();
-        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss"); /*to display date(yyyy/mm/dd)*/
         Date time = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss"); /*to display date(yyyy/mm/dd)*/
         String formattedTime = dateFormat.format(time);
         event.getEntityPlayer().sendMessage(new TextComponentString("["+formattedTime+"]: "+player+" has picked up "+item+" x"+amount));
     }
